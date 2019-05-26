c823fb909154359a39b8265e179f9bfec62ddca2
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-05-26 10:30:17 +0530

    Added "addCandidates" function and called it

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-	// Store candidate
-	// Read candidate
-	string public candidate;
-	// Constructor
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store Candidates
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates; // List of Candidates
+	// Store Candidates Count
+	uint public candidatesCount; // in a uint the default value is 0
+
 	constructor () public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string memory _name) private {
+		// This Function is private because We only need our Contract to add Candidates
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
