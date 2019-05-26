3975f1eadec388a199bc81d546c1b25099617b8c
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-05-26 12:35:16 +0530

    finished but not working

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,12 +7,17 @@ contract Election {
 		string name;
 		uint voteCount;
 	}
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
 	// Store Candidates
 	// Fetch Candidate
 	mapping(uint => Candidate) public candidates; // List of Candidates
 	// Store Candidates Count
 	uint public candidatesCount; // in a uint the default value is 0
 
+	event votedEvent(
+		uint indexed _candidateId
+	);
 	constructor () public {
 		addCandidate("Candidate 1");
 		addCandidate("Candidate 2");
@@ -22,5 +27,21 @@ contract Election {
 		// This Function is private because We only need our Contract to add Candidates
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote(uint _candidateId) public {
+		// require that they haven't voted before
+		require(!voters[msg.sender]);
+		
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		// record that voter has voted
+		voters[msg.sender] = true;
+
+		// update candidates vote count
+		candidates[_candidateId].voteCount ++;
+
+		emit votedEvent(_candidateId);
 	}
 }
